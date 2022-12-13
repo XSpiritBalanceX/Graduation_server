5b8328e17200cdad712beffc2779944c9f284f07
Storm <lenagreidon@mail.ru>
2022-12-13 15:20:39 +0300

    13.12

--- passportGoogle.js
+++ passportGoogle.js
@@ -1,11 +1,13 @@
 const passport=require('passport');
 const GoogleStrategy =require('passport-google-oauth20').Strategy;
 require('dotenv').config();
-const {MyUsers}=require('./dataBase/descriptionDB')
+const {MyUsers}=require('./dataBase/descriptionDB');
+const GOOGLE_CLIENT_ID='123235015155-rf6n17fn2vslogrejtafkgrfu65inoq2.apps.googleusercontent.com'
+const GOOGLE_CLIENT_SECRET='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'
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret:GOOGLE_CLIENT_SECRET,
     callbackURL:'http://localhost:5000/auth/google/callback'
 },
     async function(accessToken, refreshToken, profile, done) {
